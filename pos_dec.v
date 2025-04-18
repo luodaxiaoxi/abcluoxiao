8a806e5953f45f60babc54af71f03c1aa4b0c731
luodaxiao <48942383+luodaxiaoxi@users.noreply.github.com>
2025-04-19 00:23:30 +0800

    更新 pos_dec.v

--- pos_dec.v
+++ pos_dec.v
@@ -26,23 +26,23 @@ assign err_cnt = (err_vld==1)?1:0;
 always@(posedge clk or negedge rst_n)begin
     if(rst_n==1'b0)
         zero_flag <= 1'b0;
-    else if(zero_flag==0)begin
+    else if(zero_flag==0)begin//当前没有起点
         if(has_zero)
             zero_flag <=1;
         else ;
     end
-    else begin // zero_flag=1
+    else begin // zero_flag=1 当前有起点
         if(has_zero==0 & has_max=1)
             zero_flag<=0;
         else if(has_zero==1 && has_max==0)
-            zero_flag<=1; // reflash flag
+            zero_flag<=1; //又出现0，刷新起点
         else begin//科目一，只可能出现f在前，零在后。 如果零在前，f在后就出现同排了。
             zero_flag<=1;
         end
     end
 end
 
-always@(posedge clk or negedge rst_n)begin
+always@(posedge clk or negedge rst_n)begin//记录0到ffff前一拍持续时间，当ffff出现时用ffff位置+cnt去和th比较
     if(rst_n==1'b0)
         cnt <= 5'b0;
     else if(zero_flag==0)begin
@@ -81,7 +81,6 @@ always@(posedge clk or negedge rst_n)begin
     end
 end
 
-
 always@(*) begin
     err_vld=0;
     if(zero_flag==1)begin
@@ -97,6 +96,4 @@ always@(*) begin
         end
     end
 end
-
-
 endmodule
